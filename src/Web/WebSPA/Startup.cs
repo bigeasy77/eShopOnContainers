7002788b12a5c6694e0faf8df70ee2a4b5960358
Marek Wilkosz <AMW044@motorolasolutions.com>
2022-09-18 08:48:47 +0200

    items routing working

--- src/Web/WebSPA/Startup.cs
+++ src/Web/WebSPA/Startup.cs
@@ -130,7 +130,7 @@ public class Startup
             { 
 
                 // use the SpaServices extension method for angular, that will make the application to run "ng serve" for us, when in development.
-                spa.UseAngularCliServer(npmScript: "start");
+                // spa.UseAngularCliServer(npmScript: "start");
             }
         });
     }
